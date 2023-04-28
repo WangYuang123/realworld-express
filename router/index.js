5ba4de3b445f1803dbc2b6025172c16fcb16cb8f
wangyuan <wangyuan@qianlima.com>
2023-04-28 16:38:43 +0800

    feat 模版页完成

--- router/index.js
+++ router/index.js
@@ -16,5 +16,31 @@ router.get('/register', (req, res) => {
 	res.render('login.html')
 })
 
+router.get('/settings', (req, res) => {
+	res.render('settings.html')
+})
+
+router.get('/editor', (req, res) => {
+	res.render('editor.html')
+})
+
+router.get('/editor/:articledId', (req, res) => {
+	res.render('editor.html')
+})
+
+router.get('/article/:articledId', (req, res) => {
+	res.render('article.html')
+})
+
+router.get('/profile/:username', (req, res) => {
+	res.render('profile.html')
+})
+
+router.get('/profile/:username/favorites', (req, res) => {
+	res.render('profile.html')
+})
+
+
+
 
 module.exports = router;
